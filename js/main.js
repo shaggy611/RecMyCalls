c3183d6eac95d03a180c24b2d121d0f4205c5684
shaggy611 <sevczik11@gmail.com>
2021-03-19 12:42:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    (function($) {
+      var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+      $.fn.nodoubletapzoom = function() {
+        if (IS_IOS)
+          $(this).bind('touchstart', function preventZoom(e) {
+            var t2 = e.timeStamp
+              , t1 = $(this).data('lastTouch') || t2
+              , dt = t2 - t1
+              , fingers = e.originalEvent.touches.length;
+            $(this).data('lastTouch', t2);
+            if (!dt || dt > 500 || fingers > 1) return;
+    
+            e.preventDefault();
+            $(this).trigger('click').trigger('click');
+          });
+      };
+    })(jQuery);
+});  
